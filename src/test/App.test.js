7ca792895eae814bec70c40d5c5cbf548f38196b
skynikita <yangluo1105@gmail.com>
2021-04-13 13:10:49 +1000

    update todo App

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -9,15 +9,60 @@ const item_1 = {
   priority: "High",
   completed: false
 }
-test("renders add new task button", () => {
+
+
+test("fill in the New Task Form", async () =>{
   render(<App />)
-  const addNewTaskButton = screen.getByText(/Add a no Task/i)
-  expect(addNewTaskButton).toBeInTheDocument()
+  fireEvent.click(screen.getByText(/Add a New Task/i))
+  await waitFor(()=> screen.getByText(/save/i))
+  expect(screen.getByText(/save/i)).toBeInTheDocument()
+  fireEvent.change(screen.getByLabelText(/Task Name/), {
+    target: { value: "Item 1" }
+  })
+  fireEvent.change(screen.getByLabelText(/Task Description/), {
+    target: { value: "This is the first item" }
+  })
+  fireEvent.change(screen.getByLabelText(/^Priority$/), {
+    target: { value: "High" }
+  })
+  fireEvent.change(screen.getByLabelText(/Completed/), {
+    target: { checked: false }
+  })
+  await waitFor(() => screen.getByText(/Cancel/i))
+  expect(screen.getByLabelText(/Task Name/)).toHaveValue("Item 1")
+  expect(screen.getByLabelText(/Task Description/)).toHaveValue(
+      "This is the first item"
+  )
+  expect(screen.getByLabelText(/^Priority$/)).toHaveValue("High")
+  expect(screen.getByLabelText(/Completed/)).not.toBeChecked()
+  fireEvent.click(screen.getByText(/save/i))
+  await waitFor(() => screen.getByText(/Add a New Task/i))
 })
 
-test("fill in the New Task Form", () =>{
+test("delete a task", async () => {
+  window.confirm = jest.fn(() => true)
   render(<App />)
-  fireEvent.click(screen.getByText(/Add a New Task/))
-
-
+  fireEvent.click(screen.queryAllByText(/^Delete$/)[0])
+  expect(window.confirm).toBeCalledWith("Delete this task?")
+  await waitFor(() => screen.queryAllByText("Delete"))
+  expect(screen.queryAllByText(/Delete/i)).toHaveLength(4)
+  expect(screen.getByText(/total: 4 tasks/i)).toBeInTheDocument()
 })
+
+test("change sorting method", async () => {
+  render(<App />)
+  fireEvent.click(screen.getByText(/Sort By Priority/i))
+  await waitFor(() => screen.getByText(/Sort By Priority/i))
+  expect(screen.getByLabelText(/Sort By Priority/i)).toBeChecked()
+  expect(screen.getByLabelText(/Sort By name/i)).not.toBeChecked()
+  fireEvent.click(screen.getByText(/Sort By Name/i))
+  await waitFor(() => screen.getByText(/Sort By Name/i))
+  expect(screen.getByLabelText(/Sort By Name/i)).toBeChecked()
+  expect(screen.getByLabelText(/Sort By Priority/i)).not.toBeChecked()
+})
+
+
+
+
+
+
